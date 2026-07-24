fdc73a7bfc1d04c1c16a593d6cfdbd5dbd632c89
xy <hxysir@gmail.com>
2026-07-24 20:09:27 +0800

    创建项目

---
+++ src/main/java/com/sola/pickstars/PickStarsApplication.java
@@ -0,0 +1,13 @@
+package com.sola.pickstars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickStarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickStarsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sola/pickstars/PickStarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sola.pickstars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickStarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
